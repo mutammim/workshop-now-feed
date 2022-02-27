16550050bdc277db2eeba331b933f3db17d7eb9b
mutammim <58490040+mutammim@users.noreply.github.com>
2022-02-26 19:01:35 -0500

    Part 3 complete

---
+++ src/components/Search.svelte
@@ -0,0 +1,21 @@
+<form action="https://www.google.com/search" method="GET" role="search">
+	<div
+		class="rounded-lg bg-white shadow-sm hover:shadow-md p-4 flex flex-col gap-4 transition-shadow ease-in-out"
+	>
+		<div class="flex flex-col items-center gap-2">
+			<input
+				type="search"
+				name="q"
+				value=""
+				class="mt-0 block w-full p-0 border-0 border-b-2 border-slate-200 focus:ring-0 focus:border-slate-900 transition-colors ease-in-out"
+			/>
+		</div>
+		<div class="flex justify-end">
+			<button
+				type="submit"
+				class="px-4 py-2 bg-sky-500 hover:bg-sky-600 text-white rounded-lg transition-colors ease-in-out"
+				>Search</button
+			>
+		</div>
+	</div>
+</form>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,9 @@
+<script>
+	import Search from '../components/Search.svelte';
+</script>
+
 <div class="h-screen w-screen bg-slate-50 text-slate-900 selection:bg-sky-500 selection:text-white">
 	<main class="flex flex-col max-w-xl h-screen mx-auto p-4 gap-4">
-		<div
-			class="rounded-lg bg-white shadow-sm hover:shadow-md p-4 flex flex-col gap-4 transition-shadow ease-in-out"
-		>
-			<h1>Welcome to SvelteKit</h1>
-			<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-		</div>
+		<Search />
 	</main>
 </div>
