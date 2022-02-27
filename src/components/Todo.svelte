0d60d393ac3b9a4afa91762f9c870ace9d646f4b
mutammim <58490040+mutammim@users.noreply.github.com>
2022-02-26 19:14:10 -0500

    Part 4 complete

---
+++ src/components/Todo.svelte
@@ -0,0 +1,20 @@
+<div
+	class="rounded-lg bg-white shadow-sm hover:shadow-md p-4 flex flex-col gap-4 transition-shadow ease-in-out"
+>
+	<div class="flex items-center gap-2">
+		<input
+			type="checkbox"
+			class="rounded-full w-5 h-5 focus:ring-transparent text-sky-500 border-2 border-slate-300 transition-colors ease-in-out"
+		/>
+		<span>Just an example todo item</span>
+	</div>
+	<div class="flex flex-col items-center gap-2">
+		<input
+			type="text"
+			class="mt-0 block w-full p-0 border-0 border-b-2 border-slate-200 focus:ring-0 focus:border-slate-900 transition-colors ease-in-out"
+		/>
+	</div>
+	<div class="flex justify-end">
+		<button class="bg-slate-300 px-6 py-2 rounded-lg transition-colors ease-in-out">Add</button>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
+	import Todo from '../components/Todo.svelte';
 	import Search from '../components/Search.svelte';
 </script>
 
 <div class="h-screen w-screen bg-slate-50 text-slate-900 selection:bg-sky-500 selection:text-white">
 	<main class="flex flex-col max-w-xl h-screen mx-auto p-4 gap-4">
 		<Search />
+		<Todo />
 	</main>
 </div>
