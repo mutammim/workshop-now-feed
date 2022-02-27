5a56cb45fd272e9f583237427ea1be0a2d8b77a4
mutammim <58490040+mutammim@users.noreply.github.com>
2022-02-26 21:11:30 -0500

    Part 9 complete

---
+++ src/components/Weather.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let data;
+</script>
+
+<div
+	class="rounded-lg bg-white shadow-sm hover:shadow-md p-4 flex flex-col gap-4 transition-shadow ease-in-out"
+>
+	<div class="flex gap-2 justify-between px-2">
+		<div class="font-extrabold text-4xl">
+			{Math.round(data.main.temp)}°
+		</div>
+		<div class="text-slate-800 text-right">
+			<p>{data.name}</p>
+			<p class="font-bold">
+				{data.weather[0].main}
+			</p>
+		</div>
+	</div>
+	<div class="grid grid-cols-2 gap-4">
+		<div class="rounded-lg flex justify-between bg-slate-50 p-4">
+			<div>
+				{Math.round(data.main.temp_min)}°
+			</div>
+			<div class="font-bold">
+				{Math.round(data.main.temp_max)}°
+			</div>
+		</div>
+		<div class="rounded-lg bg-slate-50 p-4">
+			Feels like <span class="font-bold">
+				{Math.round(data.main.feels_like)}°
+			</span>
+		</div>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,6 +15,7 @@
 <script>
 	import Todo from '../components/Todo.svelte';
 	import Search from '../components/Search.svelte';
+	import Weather from '../components/Weather.svelte';
 
 	export let weather;
 </script>
@@ -23,7 +24,6 @@
 	<main class="flex flex-col max-w-xl h-screen mx-auto p-4 gap-4">
 		<Search />
 		<Todo />
-
-		{JSON.stringify(weather)}
+		<Weather data={weather} />
 	</main>
 </div>
