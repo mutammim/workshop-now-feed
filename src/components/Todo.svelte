603c5dc9254acb2a1ede542e33787e8a2bae1c8b
mutammim <58490040+mutammim@users.noreply.github.com>
2022-02-26 19:33:36 -0500

    Part 5 complete

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,17 +1,36 @@
+<script>
+	let allTodos = [
+		{
+			id: 1,
+			content: 'Finish homework',
+			isDone: false
+		},
+		{
+			id: 2,
+			content: 'Drink water',
+			isDone: true
+		}
+	];
+</script>
+
 <div
 	class="rounded-lg bg-white shadow-sm hover:shadow-md p-4 flex flex-col gap-4 transition-shadow ease-in-out"
 >
-	<div class="flex items-center gap-2">
-		<input
-			type="checkbox"
-			class="rounded-full w-5 h-5 focus:ring-transparent text-sky-500 border-2 border-slate-300 transition-colors ease-in-out"
-		/>
-		<span>Just an example todo item</span>
-	</div>
+	{#each allTodos as todo (todo.id)}
+		<div class="flex items-center gap-2">
+			<input
+				type="checkbox"
+				class="rounded-full w-5 h-5 focus:ring-transparent text-sky-500 border-2 border-slate-300 transition-colors ease-in-out"
+				checked={todo.isDone}
+			/>
+			<span class={todo.isDone ? 'line-through text-gray-700' : ''}>{todo.content}</span>
+		</div>
+	{/each}
+
 	<div class="flex flex-col items-center gap-2">
 		<input
 			type="text"
-			class="mt-0 block w-full p-0 border-0 border-b-2 border-slate-200 focus:ring-0 focus:border-slate-900 transition-colors ease-in-out"
+			class="mt-0 block w-full p-2 border-0 border-b-2 border-slate-200 focus:ring-0 focus:border-slate-900 transition-colors ease-in-out"
 		/>
 	</div>
 	<div class="flex justify-end">
